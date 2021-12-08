613dd462d489fe4f3b86cbbfde9eed495181cb34
Velina Chakarova <65416281+velichakarova@users.noreply.github.com>
2021-12-08 14:14:48 +0000

    nav-burgar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,8 @@
 const menuBtn= document.querySelector('.menu-btn');
 const humburger = document.querySelector('.menu-btn_burgar');
 const nav = document.querySelector('.nav');
-const menuNav= document.querySelector('.menu-nav')
+const menuNav= document.querySelector('.menu-nav');
+const navItem = document.querySelectorAll('.menu-nav_item');
 
 let showMenu = false;
 
@@ -12,12 +13,14 @@ function toggleMenu(){
         humburger.classList.add('open');
         nav.classList.add('open');
         menuNav.classList.add('open');
+        navItem.forEach(item=>item.classList.add('open'));
         showMenu = true;
 
     }else{
         humburger.classList.remove('open');
         nav.classList.remove('open');
         menuNav.classList.remove('open');
+        navItem.forEach(item=>item.classList.remove('open'));
         showMenu = false;
     }
 }
